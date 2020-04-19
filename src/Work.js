73b15e363b96574a3966f5577433bb2981f9ee1d
Jaskirat Singh Oberoi <Jso101196@gmail.com>
2020-04-19 16:48:52 +0530

    added hanu material

--- src/Work.js
+++ src/Work.js
@@ -19,13 +19,39 @@ export default function Work() {
             title={"Hanu Software"}
             subtitle={"Cloud Engineer"}
           >
-            dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum.
+            &#10687; Project 1 (Developing Data ETL Platform for future
+            projects) - Client Company 1 [$18bn total revenue, 2017-2018]
+            <br />• Deployed a new Data platform which, on completion, will save
+            the company millions of dollars as they will have a proper system to
+            onboard new data ETL and analysis projects along with a secure data
+            governance, compliance and quality ruleset
+            <br />• Developing Pipelines, Linked Services, Triggers, Datasets in
+            Azure Data Factory
+            <br />• Developing automated scripts for scaling of SQL Data
+            Warehouse and Database Implementation of Azure Synapse Analytics
+            Data modelling concepts, i.e., Fact and Dimensions etc
+            <br />• Implementing Data ingestion from sources like flat files,
+            Azure Data Lake Gen 2, APIs etc into Synapse Analytics
+            <br />
+            <br />
+            &#10687; Project 2 (ETL of Sales Data) – Client Company 2 [$4.2bn
+            total revenue, 2018-2019]
+            <br />• Implementing Data ingestion from sources like flat files,
+            On-Prem SQL Server etc. through Azure Data Factory into Azure
+            Synapse Analytics
+            <br />• Transformation of unstructured ingested data using python in
+            Azure Databricks
+            <br />• Support in developing BI reports using PowerBI
+            <br />
+            <br />
+            &#10687; Project 3 (Chatbot) – Client Company 3 [$350M total
+            revenue, 2018-2019]
+            <br />• Implemented UX principles in the current design to enhance
+            its functionality
+            <br />• Created an interactive front-end prototype for the chatbot
+            admin portal using Adobe XD tool
+            <br />• Designed the complete front-end prototype from scratch using
+            HTML5, Sass and JavaScript
           </Event>
           <Event
             interval={"May 2018 – July 2018"}
@@ -36,11 +62,10 @@ export default function Work() {
             Developed desktop app that is a Question Recommender for Stack
             Overflow:
             <br />
-            &#10687; Core framework based on data mining using Jaccard’s Index
+            • Core framework based on data mining using Jaccard’s Index
             implemented in Python <br />
-            &#10687; Sample dataset imported from Kaggle
-            <br />
-            &#10687; Front-end UI of the windows application was developed using
+            • Sample dataset imported from Kaggle
+            <br />• Front-end UI of the windows application was developed using
             TKinter
           </Event>
         </Timeline>
