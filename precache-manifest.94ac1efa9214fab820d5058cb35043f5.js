356087fc02bcc3a0107dab8b84d5ffe4c1257890
JaskiratOberoi <Jso101196@gmail.com>
2024-01-07 13:34:35 +0000

    Build: (ad8bcba) Fixed publish.yml

---
+++ precache-manifest.94ac1efa9214fab820d5058cb35043f5.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41282356868a087c923de1552d9118af",
+    "url": "https://jaskiratoberoi.com/index.html"
+  },
+  {
+    "revision": "9422852e30f2b53242ab",
+    "url": "https://jaskiratoberoi.com/static/css/main.e7924665.chunk.css"
+  },
+  {
+    "revision": "5cd8837b59d978b2f351",
+    "url": "https://jaskiratoberoi.com/static/js/2.f5c47eb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://jaskiratoberoi.com/static/js/2.f5c47eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9422852e30f2b53242ab",
+    "url": "https://jaskiratoberoi.com/static/js/main.2cf1ed1f.chunk.js"
+  },
+  {
+    "revision": "c5f0b13e9637866a7c19",
+    "url": "https://jaskiratoberoi.com/static/js/runtime-main.153affe2.js"
+  },
+  {
+    "revision": "895a094c9e1696dc02663082a58f585a",
+    "url": "https://jaskiratoberoi.com/static/media/022-website.895a094c.svg"
+  },
+  {
+    "revision": "648b96e43bf240e467fc3e2194a0f20c",
+    "url": "https://jaskiratoberoi.com/static/media/033-vscode.648b96e4.svg"
+  },
+  {
+    "revision": "2f8095234c7ff49c61e83918047d2d11",
+    "url": "https://jaskiratoberoi.com/static/media/040-website.2f809523.svg"
+  },
+  {
+    "revision": "8fe78d1c9764cd83b154efb3bbdd99c6",
+    "url": "https://jaskiratoberoi.com/static/media/All Resources.8fe78d1c.svg"
+  },
+  {
+    "revision": "e82d6a2453a45f3420828db05e7ebdd9",
+    "url": "https://jaskiratoberoi.com/static/media/App Services.e82d6a24.svg"
+  },
+  {
+    "revision": "4edce7037f3bc4602e297884b7efb6e2",
+    "url": "https://jaskiratoberoi.com/static/media/Azure Databricks.4edce703.svg"
+  },
+  {
+    "revision": "b575baef0d2e5b3091c04c3338a839e1",
+    "url": "https://jaskiratoberoi.com/static/media/Data Factories.b575baef.svg"
+  },
+  {
+    "revision": "d87a0f7da4755f2f0fd177459174c8ba",
+    "url": "https://jaskiratoberoi.com/static/media/Data Lake Storage.d87a0f7d.svg"
+  },
+  {
+    "revision": "57f55d66a4331d2a1ccc64eb44244876",
+    "url": "https://jaskiratoberoi.com/static/media/Figma.57f55d66.svg"
+  },
+  {
+    "revision": "39675f321b60a7c24a1ad677d522cf17",
+    "url": "https://jaskiratoberoi.com/static/media/SQL Databases.39675f32.svg"
+  },
+  {
+    "revision": "10e321e2929228e9ec7a400755b78667",
+    "url": "https://jaskiratoberoi.com/static/media/bs4.10e321e2.svg"
+  },
+  {
+    "revision": "d28d3f1baaa171432748800cdd97ee0f",
+    "url": "https://jaskiratoberoi.com/static/media/call.d28d3f1b.svg"
+  },
+  {
+    "revision": "bbef1469fe869ba1ed270269a6cf2b80",
+    "url": "https://jaskiratoberoi.com/static/media/choose.bbef1469.png"
+  },
+  {
+    "revision": "a6cc690ddc57b6e2eb4b56abd37d2080",
+    "url": "https://jaskiratoberoi.com/static/media/cmr.a6cc690d.png"
+  },
+  {
+    "revision": "b6a60da3642913f2455806d0b752bbfd",
+    "url": "https://jaskiratoberoi.com/static/media/code.b6a60da3.svg"
+  },
+  {
+    "revision": "f4c3cb3c1142fa7097b6d868e59f2594",
+    "url": "https://jaskiratoberoi.com/static/media/covid.f4c3cb3c.png"
+  },
+  {
+    "revision": "02ba08f6f4ba60fa4110724b2abc3d2c",
+    "url": "https://jaskiratoberoi.com/static/media/covidjs.02ba08f6.png"
+  },
+  {
+    "revision": "9a35599f78ac216570cd22afcfe55e60",
+    "url": "https://jaskiratoberoi.com/static/media/csi.9a35599f.png"
+  },
+  {
+    "revision": "7e46efde04ebb7e27356491336f3237c",
+    "url": "https://jaskiratoberoi.com/static/media/flask.7e46efde.svg"
+  },
+  {
+    "revision": "d10df85e7cfabaa9c17da2ef2eae9ce2",
+    "url": "https://jaskiratoberoi.com/static/media/git-icon.d10df85e.svg"
+  },
+  {
+    "revision": "ad7a2904f9e503047633559a09b2b105",
+    "url": "https://jaskiratoberoi.com/static/media/github.ad7a2904.svg"
+  },
+  {
+    "revision": "8e7506166d49ef6542c52e12daf14544",
+    "url": "https://jaskiratoberoi.com/static/media/hanseibydesign.8e750616.png"
+  },
+  {
+    "revision": "6638e24f5297998e69c94339cdd5df28",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-adobe-photoshop.6638e24f.svg"
+  },
+  {
+    "revision": "e7f43c4156e0bd569a76575baaef2caa",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-adobe-xd.e7f43c41.svg"
+  },
+  {
+    "revision": "aacd65890a2a867b67f39d1f4ef41e12",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-css3.aacd6589.svg"
+  },
+  {
+    "revision": "46cb3d462db2a04d7b7fa84061bd616f",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-html-5.46cb3d46.svg"
+  },
+  {
+    "revision": "3d875ed05ad67682aa8afac1838b8c6c",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-javascript.3d875ed0.svg"
+  },
+  {
+    "revision": "add8df8dc5f78ecbf8bfea7d3fa187dc",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-python.add8df8d.svg"
+  },
+  {
+    "revision": "652f28d1787dcf20fed8d848db36fed1",
+    "url": "https://jaskiratoberoi.com/static/media/icons8-react-native.652f28d1.svg"
+  },
+  {
+    "revision": "4d3a8761d0a88e2eb79afb019c22afb6",
+    "url": "https://jaskiratoberoi.com/static/media/jaskirat-9.4d3a8761.png"
+  },
+  {
+    "revision": "a0bfcdf9cb9fc6e0f941e2660a9d1964",
+    "url": "https://jaskiratoberoi.com/static/media/life.a0bfcdf9.png"
+  },
+  {
+    "revision": "b3cb857b8a8802d4dd84f73aec49e494",
+    "url": "https://jaskiratoberoi.com/static/media/mail.b3cb857b.svg"
+  },
+  {
+    "revision": "81145c3f70c7af7dee66ad2415f32adb",
+    "url": "https://jaskiratoberoi.com/static/media/map.81145c3f.svg"
+  },
+  {
+    "revision": "a6ded22e91d053b40a3a14908545ccfc",
+    "url": "https://jaskiratoberoi.com/static/media/mori.a6ded22e.png"
+  },
+  {
+    "revision": "b8fcaae2c8f69a3be3a684744474be82",
+    "url": "https://jaskiratoberoi.com/static/media/portfolio.b8fcaae2.png"
+  },
+  {
+    "revision": "1023f78a2492cb3e48519909df2f1919",
+    "url": "https://jaskiratoberoi.com/static/media/sass.1023f78a.svg"
+  },
+  {
+    "revision": "b0e48af5354fcaaf6fd51e2e12acd212",
+    "url": "https://jaskiratoberoi.com/static/media/sql.b0e48af5.svg"
+  },
+  {
+    "revision": "ad4c71d229f1d65c1cabcd57f3f9d811",
+    "url": "https://jaskiratoberoi.com/static/media/tft.ad4c71d2.png"
+  },
+  {
+    "revision": "161ce391c4b194525de65c374fa17956",
+    "url": "https://jaskiratoberoi.com/static/media/todo.161ce391.png"
+  },
+  {
+    "revision": "43e86b7f29d82f12d171a0495a39fbe6",
+    "url": "https://jaskiratoberoi.com/static/media/ttt.43e86b7f.png"
+  },
+  {
+    "revision": "8af0624dd517e369cf9d511320eab196",
+    "url": "https://jaskiratoberoi.com/static/media/tvs.8af0624d.png"
+  },
+  {
+    "revision": "51f15234690220f21c59cdfcf9f7a413",
+    "url": "https://jaskiratoberoi.com/static/media/ui.51f15234.svg"
+  },
+  {
+    "revision": "f7f1ecc8ea4dfe7ff81f79d3ea16f0bd",
+    "url": "https://jaskiratoberoi.com/static/media/uxwriting.f7f1ecc8.svg"
+  },
+  {
+    "revision": "72a19cbb3606067022c3a20771b72a38",
+    "url": "https://jaskiratoberoi.com/static/media/wireframe.72a19cbb.svg"
+  }
+]);
