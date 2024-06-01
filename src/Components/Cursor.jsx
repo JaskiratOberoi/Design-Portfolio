40ecade78c3ceed2b8162af973e13d5d317d3492
Jaskirat Oberoi <jooberoi@amazon.com>
2024-06-01 21:15:41 +0530

    cursor fix

--- src/Components/Cursor.jsx
+++ src/Components/Cursor.jsx
@@ -1,43 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { motion } from "framer-motion/dist/framer-motion";
+// import React, { useEffect, useState } from 'react'
+// import { motion } from "framer-motion/dist/framer-motion";
 
-function Cursor() {
-    const[mousePosition, setMousePosition] = useState({
-        x: 0,
-        y: 0
-    });
+// function Cursor() {
+//     const[mousePosition, setMousePosition] = useState({
+//         x: 0,
+//         y: 0
+//     });
 
-    const [cursorVariant, 
-        // setCursorVariant
-    ] = useState("default");
+//     const [cursorVariant, 
+//         // setCursorVariant
+//     ] = useState("default");
 
-    useEffect(() => {
+//     useEffect(() => {
 
-        const mouseMove = e => {
-            setMousePosition({
-                x: e.clientX,
-                y: e.clientY
-            })
-        }
-        window.addEventListener("mousemove", mouseMove)
+//         const mouseMove = e => {
+//             setMousePosition({
+//                 x: e.clientX,
+//                 y: e.clientY
+//             })
+//         }
+//         window.addEventListener("mousemove", mouseMove)
 
-        return () => {
-            window.removeEventListener("mousemove", mouseMove);
-        }
-    }, [])
+//         return () => {
+//             window.removeEventListener("mousemove", mouseMove);
+//         }
+//     }, [])
 
-    const variants = {
-        default: {
-            x: mousePosition.x - 12,
-            y: mousePosition.y - 12
-        }
-    }
-  return (
-    <motion.div className='cursor'
-    variants={variants}
-    animate={cursorVariant}
-    />
-  )
-}
+//     const variants = {
+//         default: {
+//             x: mousePosition.x - 12,
+//             y: mousePosition.y - 12
+//         }
+//     }
+//   return (
+//     <motion.div className='cursor'
+//     variants={variants}
+//     animate={cursorVariant}
+//     />
+//   )
+// }
 
-export default Cursor
+// export default Cursor
